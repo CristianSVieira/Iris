da6df443f3cd284ca2b7e2809fa63a90889f11fb
Cristian <vieira.cristian7913@gmail.com>
2026-03-09 21:11:26 -0300

    Add video and image

--- mob/App.tsx
+++ mob/App.tsx
@@ -1,11 +1,52 @@
+import { useEvent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import React from 'react';
+import { useRef } from 'react';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+
+const videoSource = require('./background.mp4');
 
 export default function App() {
+
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.play();
+  });
+
+  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>LSVilantino!</Text>
       <StatusBar style="light" />
+
+      <Image
+        style={styles.reactimagem}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+
+      <VideoView
+        style={styles.samplevideo}
+        player={player}
+        fullscreenOptions={{ enable: true }}
+        allowsPictureInPicture
+      />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={isPlaying ? 'Pause' : 'Play'}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
+          }}
+        />
+      </View>
+
     </View>
   );
 }
@@ -20,5 +61,19 @@ const styles = StyleSheet.create({
 
   text: {
     color: 'white'
-  }
+  },
+
+  reactimagem: {
+    width: 250,
+    height: 250
+  },
+
+  samplevideo: {
+    width: 350,
+    height: 275,
+  },
+
+  controlsContainer: {
+    padding: 10,
+  },
 });
